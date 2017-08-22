30ddd2c60805a94dea81ee13dc1816649731f674
marina31 <marina.rouille@gmail.com>
2017-08-22 16:21:17 +0200

    Mise en place initiale

---
+++ README.md
@@ -0,0 +1,13 @@
+# aeris-commons-fonts
+
+> Aeris commons fonts. 
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# generate fonts for icons
+npm run font
+
---
+++ script/gensvgfont.js
@@ -0,0 +1,17 @@
+const webfontsGenerator = require('webfonts-generator');
+var path = require('path')
+ 
+webfontsGenerator({
+  files: [
+	  path.resolve(__dirname, './../svg/chamber.svg'),
+	  path.resolve(__dirname, './../svg//experiment.svg')
+  ],
+  dest: path.resolve(__dirname, './../font/'),
+  fontName: 'aeriscommmonsfonts',
+}, function(error) {
+  if (error) {
+    console.log('Fail!', error);
+  } else {
+    console.log('Done!');
+  }
+})
