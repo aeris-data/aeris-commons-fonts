0e7606fcdc4a40473d50bd8e4c93b647b32fd016
marina31 <marina.rouille@gmail.com>
2017-08-23 10:28:11 +0200

    Add catalog icon and prefix aeris

--- script/gensvgfont.js
+++ script/gensvgfont.js
@@ -5,10 +5,23 @@ webfontsGenerator({
   files: [
 	  path.resolve(__dirname, './../svg/chamber.svg'),
 	  path.resolve(__dirname, './../svg//experiment.svg'),
-	  path.resolve(__dirname, './../svg//wall.svg')
+	  path.resolve(__dirname, './../svg//wall.svg'),
+	  path.resolve(__dirname, './../svg//ionization.svg'),
+	  path.resolve(__dirname, './../svg//resolution.svg'),
+	  path.resolve(__dirname, './../svg//spectrum.svg'),
+	  path.resolve(__dirname, './../svg//aircraft.svg'),
+	  path.resolve(__dirname, './../svg//balloon.svg'),
+	  path.resolve(__dirname, './../svg//eos.svg'),
+	  path.resolve(__dirname, './../svg//insitulandplatform.svg'),
+	  path.resolve(__dirname, './../svg//spacestation.svg'),
+	  path.resolve(__dirname, './../svg//unknown.svg')
   ],
   dest: path.resolve(__dirname, './../font/'),
-  fontName: 'aeriscommmonsfonts',
+  templateOptions : {
+	  classPrefix: 'aeris-icon-',
+	  baseSelector: '.aeris-icon'
+  },
+  fontName: 'aeriscommmonsfonts'
 }, function(error) {
   if (error) {
     console.log('Fail!', error);
